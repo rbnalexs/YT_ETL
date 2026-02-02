b0db16defc5d3bd3edcbd7373e48f7047285ae31
Ruben Sanchez <rubensanchezcolin@gmail.com>
2026-02-02 01:00:50 -0600

    video_stats

---
+++ video_stats.py
@@ -0,0 +1,26 @@
+import requests
+import json
+API_KEY = "AIzaSyAkOy-liCW0J9tnbp-_3zGW92b0hptyWhE"
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_id():
+    
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        #print(json.dumps(data, indent=4))
+        channel_items = data['items'][0]
+        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        #print(channel_playlistId)
+        return channel_playlistId
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    #print("get_playlist_id will be executed")
+    get_playlist_id()
+else:
+    print("get_playlist_id won't be executed")
