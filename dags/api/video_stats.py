13fc46856cd3a1f3f741e806bfaf063af6a8d80f
Ruben Sanchez <rubensanchezcolin@gmail.com>
2026-02-09 21:10:22 -0600

    docker compose

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,16 +2,23 @@ import requests
 import json
 from datetime import date
 
-import os 
-from dotenv import load_dotenv
+# import os 
+# from dotenv import load_dotenv
+# from airflow.decorators import task
 
+#load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+#API_KEY = os.getenv("API_KEY")
+#CHANNEL_HANDLE = "MrBeast"
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+from airflow.decorators import task
+from airflow.models import Variable
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("MrBeast")
 maxResults = 10
 
+@task
 def get_playlist_id():
     
     try:
@@ -28,6 +35,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def get_video_ids(playlistId):
     video_ids = []
     pageToken = None
@@ -54,6 +62,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -97,13 +106,12 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
     with open(file_path, "w", encoding="utf-8") as json_outfile:
         json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
-
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
