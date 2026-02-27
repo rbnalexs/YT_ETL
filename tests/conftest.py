66b7cc048aaaa6d63d49a1901fd92d9975132a08
Ruben Sanchez <rubensanchezcolin@gmail.com>
2026-02-26 18:16:49 -0600

    Pruebas unitarias

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+import psycopg2
+from unittest import mock
+from airflow.models import Variable, Connection, DagBag
+
+
+@pytest.fixture
+def api_key():
+    with mock.patch.dict("os.environ", AIRFLOW_VAR_API_KEY="MOCK_KEY1234"):
+        yield Variable.get("API_KEY")
+        
+
+@pytest.fixture
+def channel_handle():
+    with mock.patch.dict("os.environ", AIRFLOW_VAR_CHANNEL_HANDLE="MRCHEESE"):
+        yield Variable.get("CHANNEL_HANDLE")
---
+++ tests/unit_test.py
@@ -0,0 +1,5 @@
+def test_api_key(api_key):
+    assert api_key == "MOCK_KEY1234"
+
+def test_channel_handle(channel_handle):
+    assert channel_handle == "MRCHEESE"
